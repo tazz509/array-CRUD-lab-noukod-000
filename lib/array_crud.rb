554675e0313b5bdaca07fed6856b46394c655b78
carly <carly@noukod.com>
2018-09-19 17:29:40 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,25 +1,25 @@
 def create_an_empty_array
-  
+  array.new
 end
 
 def create_an_array
-  
+  array = [a,b,c]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.unshift element
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array.pop element
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.shift element
 end
 
 def retrieve_element_from_index(array, index_number)
